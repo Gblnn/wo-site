cc2594632c4bfa2d84a799342f67b8d9392d9693
Gokul <122973682+Gblnn@users.noreply.github.com>
2025-02-02 17:05:33 +0400

    Update landing.tsx

--- src/pages/sections/landing.tsx
+++ src/pages/sections/landing.tsx
@@ -51,7 +51,8 @@ export default function Landing() {
 
         <ChevronDown
           className="animate-bounce"
-          width={"1.5rem"}
+          width={"2rem"}
+          height={"2rem"}
           color="orangered"
           style={{ position: "absolute", marginTop: "22rem" }}
         />
