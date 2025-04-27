c8dd181e9aac7323432e4561b50941736d2f4d10
Gokul <122973682+Gblnn@users.noreply.github.com>
2025-04-27 17:08:08 +0400

    Revert "update"
    This reverts commit 557594a0d113d570f98c6f27121e1caac2e17c78.

--- vite.config.ts
+++ vite.config.ts
@@ -12,32 +12,5 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
-    assetsDir: "assets",
-    cssCodeSplit: true,
-    minify: "terser",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom", "framer-motion"],
-        },
-        assetFileNames: (assetInfo) => {
-          const name = assetInfo.name || "";
-          const info = name.split(".");
-          const ext = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return `assets/images/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-        chunkFileNames: "assets/[name]-[hash].js",
-        entryFileNames: "assets/[name]-[hash].js",
-      },
-    },
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
   },
 });
